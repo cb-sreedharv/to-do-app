5016f497f3408e9f7c3e92ecd0f2583c2f78a356
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-24 15:59:12 +0530

    Refactored Code to follow Google Checks

--- src/main/java/com/interns/todo/ToDoApplication.java
+++ src/main/java/com/interns/todo/ToDoApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+/**
+ * Spring Boot Application Class.
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ToDoApplication {
-    public static void main(final String[] args) {
-        SpringApplication.run(ToDoApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ToDoApplication.class, args);
+  }
 }
+
