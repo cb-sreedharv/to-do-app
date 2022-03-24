249b912c0ca9d0c5692d21b42b96f5717d281553
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-24 15:13:17 +0530

    Initial Commit

---
+++ src/main/java/com/interns/todo/ToDoApplication.java
@@ -0,0 +1,12 @@
+package com.interns.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ToDoApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(ToDoApplication.class, args);
+    }
+}
