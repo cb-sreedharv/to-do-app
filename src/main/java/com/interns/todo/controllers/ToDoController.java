b4f38158a6ad99752ef00c479b4da8e22ff60801
cb-charitha-erikireddy <102163240+cb-charitha-erikireddy@users.noreply.github.com>
2022-03-28 16:02:52 +0530

    added todo controller

--- src/main/java/com/interns/todo/controllers/ToDoController.java
+++ src/main/java/com/interns/todo/controllers/ToDoController.java
@@ -3,6 +3,9 @@ package com.interns.todo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *ToDoController.
+ */
 @RestController
 public class ToDoController {
   @GetMapping("/")
