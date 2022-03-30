69a9a382d7da697e79600afa6f25303ea1bd5376
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-31 02:02:03 +0530

    Added update and delete functionality with route matching

--- src/main/java/com/interns/todo/controllers/ToDoController.java
+++ src/main/java/com/interns/todo/controllers/ToDoController.java
@@ -1,17 +1,23 @@
 package com.interns.todo.controllers;
 
+import com.interns.todo.exceptions.TaskNotFoundException;
 import com.interns.todo.model.Task;
 import com.interns.todo.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *ToDoController.
+ * ToDoController.
  */
 @RestController
 public class ToDoController {
@@ -35,13 +41,54 @@ public class ToDoController {
    * @param task - Task to be added to the ToDo List.
    */
   @PostMapping("/todo")
-  public ResponseEntity createToDo(@RequestBody Task task) {
+  public ResponseEntity createTask(@RequestBody Task task) {
     try {
       toDoService.createTask(task);
       return new ResponseEntity(HttpStatus.OK);
+    } catch (DataIntegrityViolationException e) {
+      return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     } catch (Exception e) {
-      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
+  /**
+   * Method to update a Task.
+   *
+   * @param id - task object containing task id and new task description.
+   * @param taskDesc - new task description.
+   */
+  @PutMapping("/todo/{taskId}")
+  public ResponseEntity updateTask(@PathVariable("taskId") long id, @RequestBody String taskDesc) {
+    try {
+      toDoService.updateTask(id, taskDesc);
+      return new ResponseEntity(HttpStatus.OK);
+    } catch (TaskNotFoundException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (Exception e) {
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  /**
+   * Method to update a Task.
+   *
+   * @param id - id of the task to be deleted.
+   */
+  @DeleteMapping("/todo/{taskId}")
+  public ResponseEntity deleteTask(@PathVariable("taskId") long id) {
+    try {
+      toDoService.deleteTask(id);
+      return new ResponseEntity(HttpStatus.OK);
+    } catch (TaskNotFoundException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    } catch (Exception e) {
+      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @RequestMapping(value = "**")
+  public ResponseEntity handleOtherRoutes() {
+    return new ResponseEntity(HttpStatus.NOT_FOUND);
   }
 }
--- src/main/java/com/interns/todo/service/ToDoService.java
+++ src/main/java/com/interns/todo/service/ToDoService.java
@@ -1,8 +1,10 @@
 package com.interns.todo.service;
 
+import com.interns.todo.exceptions.TaskNotFoundException;
 import com.interns.todo.model.Task;
 import com.interns.todo.repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,12 +16,44 @@ public class ToDoService {
   @Autowired
   ToDoRepository toDoRepository;
 
-  public void createTask(Task task) throws Exception {
+  public void createTask(Task task) throws DataIntegrityViolationException, Exception {
     toDoRepository.save(task);
   }
 
   public Iterable<Task> getTasks() {
     return toDoRepository.findAll();
+  }
+
+  /**
+   * Method to update task.
+   *
+   * @param id - task object containing task id and new task description.
+   * @param taskDesc - new task description.
+   * @throws TaskNotFoundException - Task with mentioned id does not exist in database.
+   * @throws Exception - for Internal Server Errors.
+   */
+  public void updateTask(long id, String taskDesc) throws TaskNotFoundException, Exception {
+    if (!toDoRepository.findById(id).isPresent()) {
+      throw new TaskNotFoundException("Task does not exist");
+    }
+    Task task = new Task();
+    task.setTaskId(id);
+    task.setTaskDesc(taskDesc);
+    toDoRepository.save(task);
+  }
+
+  /**
+   * Method to update task.
+   *
+   * @param id - id of the task to be deleted.
+   * @throws TaskNotFoundException - Task with mentioned id does not exist in database.
+   * @throws Exception - for Internal Server Errors.
+   */
+  public void deleteTask(long id) throws TaskNotFoundException, Exception {
+    if (!toDoRepository.findById(id).isPresent()) {
+      throw new TaskNotFoundException("Task does not exist");
+    }
+    toDoRepository.deleteById(id);
   }
 
 }
