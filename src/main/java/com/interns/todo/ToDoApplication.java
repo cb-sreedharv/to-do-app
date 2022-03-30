becbbce77a8bb33ac08fdcdba078988fc588b27d
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-03-31 00:16:09 +0530

    Sending HTTP Codes as Response

--- src/main/java/com/interns/todo/ToDoApplication.java
+++ src/main/java/com/interns/todo/ToDoApplication.java
@@ -2,7 +2,6 @@ package com.interns.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Spring Boot Application Class.
