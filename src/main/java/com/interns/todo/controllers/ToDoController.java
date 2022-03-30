0eae2cb1a446ef0b00c946994ddc2b1cf9eeca99
cb-charitha-erikireddy <102163240+cb-charitha-erikireddy@users.noreply.github.com>
2022-03-30 10:48:15 +0530

    ToDoApp

--- src/main/java/com/interns/todo/controllers/ToDoController.java
+++ src/main/java/com/interns/todo/controllers/ToDoController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ToDoController {
   @GetMapping("/")
   public String getToDo() {
-    return "Todocontroller";
+    return "ToDoApp";
   }
 }
